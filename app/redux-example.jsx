bb1ceb9f1c9bb4d75a49451f680431217e218b3a
Scott <sekorn@gmail.com>
2016-07-08 11:53:53 -1000

    add actions for adding/removing hobbies and movies

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,7 +2,16 @@ var redux = require('redux');
 
 console.log('staring redux example');
 
-var reducer = (state = {name: 'Anonymous'}, action) => {
+var stateDefault = {
+  name: 'Anonymouse',
+  hobbies: [],
+  movies: []
+};
+
+var nextHobbyId = 1;
+var nextMovieId = 1;
+
+var reducer = (state = stateDefault, action) => {
   // ES5 Code
   //state = state || {name: 'Anonymous'};
 
@@ -12,8 +21,41 @@ var reducer = (state = {name: 'Anonymous'}, action) => {
         ...state,
         name: action.name
       };
-      default:
-        return state;
+    case 'ADD_HOBBY':
+      return {
+        ...state,
+        hobbies: [
+          ...state.hobbies,
+          {
+            id: nextHobbyId++,
+            hobby: action.hobby
+          }
+        ]
+      };
+    case 'REMOVE_HOBBY':
+      return {
+        ...state,
+        hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
+      };
+    case 'ADD_MOVIE':
+      return {
+        ...state,
+        movies: [
+          ...state.movies,
+          {
+            id: nextMovieId++,
+            title: action.movie,
+            genre: action.genre
+          }
+        ]
+      };
+    case 'REMOVE_MOVIE':
+      return {
+        ...state,
+        movies: state.movies.filter((movie) => movie.id !== action.id)
+      };
+    default:
+      return state;
   }
 
   return state;
@@ -26,18 +68,49 @@ var store = redux.createStore(reducer, redux.compose(
 var unsubscribe = store.subscribe(() => {
   var state = store.getState();
 
-  console.log('Name is', state.name);
+  console.log('New state', store.getState());
   document.getElementById('app').innerHTML = state.name;
 
 });
 //unsubscribe();
 
 var currentState = store.getState();
-console.log('currentState', currentState);
 
 store.dispatch({
   type: 'CHANGE_NAME',
   name: 'Snoogins'
+});
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Running'
+});
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Walking'
+});
+
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 2
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  movie: 'Better Off Dead',
+  genre: 'Comedy'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  movie: 'Mad Max',
+  genre: 'Action'
+});
+
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id: 1,
 });
 
 store.dispatch({
